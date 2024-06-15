95f5e717366de55ed546f9193e0d8053f3c39c86
wheerok <hhww0328@gmail.com>
2024-06-15 17:57:44 +0900

    최종인데 최종 아님

--- BullsandCowsGame/main.swift
+++ BullsandCowsGame/main.swift
@@ -51,25 +51,66 @@ print(randomNum)
 print("게임을 시작합니다. 세 자리 숫자를 입력하세요.")
 
 while (true) {
-    let input = readLine()
-    guard let input = input else {
-        print ("입력한 숫자가 잘못되었습니다. 다시 시도하세요.")
-        continue
-    }
+var input = readLine()!
+var userInput :[Int] = []
     
     if input.count != 3 {
-        print("입력한 숫자가 잘못되었습니다 세자리 숫자로 해주세요.")
-    } else if let inputValue = Int(input) {
-        print("올바른 숫자를 입력하셨습니다. 게임을 시작하겠습니다!")
-    } else {
-        print("잘못된 입력입니다 숫자를 입력해주세요.")
-        //일반적인 문자가 들어오면 그걸 Int로 변환할 없기 때문에
+        print("입력한 값이 잘못되었습니다 세자리 숫자로 해주세요.")
+        continue
     }
-    
-    for x in input{
+
+    var thisAllDigit = true
+    // 초기값 설정. 왜냐하면 일단 모든 값이 숫자라고 가정하기 위해서
+
+    for char in input{// input에 담긴 모든 것을 char로 받아서 하나하나 검사함
+        if !char.isNumber{ //char 안에 있는 값이 숫자가 아니라면
+            thisAllDigit = false //발견 후 false로 재할당하고 for문 종료
+            break
+        }
+    }
+
+    //   char.isNumber
+    //    isNumber는 chasracter타입의 속성으로 이 문자가 숫자인지 아닌지 확인한다.
+    //    예를들어 char가 5면 true를 반환하고, char가 a면 false를 반환한다.
+
+    if !thisAllDigit {
+        print("입력한 값에 문자가 있습니다. 세 자리 숫자로 다시 입력하세요.")
+    }
+    // 그리고 넘어와서 !thisAllDigit 즉 true가 아닌 false임 위에서 input의 입력값을 하나하나 검사했으니 만약 false라는 것을 얻고 for문을 빠져나오면
+
+
+
+    for x in input {
+        if let num = Int(String(x)) {
+            userInput.append(num)
+            
+            if userInput.contains(0){
+                print("입력한 값에 0이 포함되어 있습니다. 다시 입력하세요.")
+                break
+            }
+        }
         
+        print(userInput)
     }
-    
+
+    var strike = 0
+    var ball = 0
+
+    for (index, number) in userInput.enumerated() {
+        
+        if number == randomNum[index] {
+            strike += 1
+        } else if userInput.contains(number){
+            ball += 1
+        }
+        
+        if strike == 3{
+            break
+        }
+        print("\(strike)스트라이크, \(ball)볼 입니다.")
+            
+    }
+
     
     
     //길이 확인하기
